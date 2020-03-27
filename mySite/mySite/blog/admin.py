22bf6e55ec789a77f0c1b033ab787d54606ab808
Jhonathas César <jhonathascesar232@gmail.com>
2020-03-27 17:55:14 -0300

    My Django Girls app, first commit

---
+++ mySite/mySite/blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Post
+# Register your models here.
+
+admin.site.register(Post) #Para tornar nosso modelo visível na página de administração,
---
+++ mySite/mySite/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+    autor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(default=timezone.now)
+    data_pub = models.DateTimeField(blank=True, null=True)
+
+    def publicar(self):
+        self.data_pub = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
