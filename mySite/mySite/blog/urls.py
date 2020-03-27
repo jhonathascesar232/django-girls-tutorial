4f1af631b8538e10046dcacbf01dbbdf8dcc089f
Jhonathas César <jhonathascesar232@gmail.com>
2020-03-27 19:44:12 -0300

    NULL

--- mySite/mySite/blog/urls.py
+++ mySite/mySite/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, listar_post
 
 urlpatterns = [
     path('', index, name = 'index'),
+    path('lista/', listar_post, name='post_list'),
 ]
--- mySite/mySite/blog/views.py
+++ mySite/mySite/blog/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+def listar_post(request):
+    data = {}
+    return render(request, 'post/list.html', data)
