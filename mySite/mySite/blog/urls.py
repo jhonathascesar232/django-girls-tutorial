548b816e5dd1d8fcfd6e7fdd8fc040cd1cf329c9
Jhonathas César <jhonathascesar232@gmail.com>
2020-03-27 21:34:52 -0300

    Configurado os arquivos estático no server

--- mySite/mySite/blog/urls.py
+++ mySite/mySite/blog/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path
 from .views import index, listar_post, post_detalhes
 
@@ -5,4 +8,4 @@ urlpatterns = [
     path('', index, name = 'index'),
     path('lista/', listar_post, name='post_list'),
     path('post-detalhes/<int:pk>/', post_detalhes, name='post_detail'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
