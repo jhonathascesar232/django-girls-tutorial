fd62822dc1e12ea6dcd15654138721a4291ae8a2
Jhonathas César <jhonathascesar232@gmail.com>
2020-03-27 21:05:23 -0300

    Finalizando tutorial

--- mySite/mySite/blog/urls.py
+++ mySite/mySite/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, listar_post
+from .views import index, listar_post, post_detalhes
 
 urlpatterns = [
     path('', index, name = 'index'),
     path('lista/', listar_post, name='post_list'),
+    path('post-detalhes/<int:pk>/', post_detalhes, name='post_detail'),
 ]
--- mySite/mySite/blog/views.py
+++ mySite/mySite/blog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
+
 
 # Create your views here.
 def index(request):
@@ -6,4 +9,14 @@ def index(request):
 
 def listar_post(request):
     data = {}
+    post = Post.objects.filter(data_pub__lte=timezone.now()).order_by('data_pub')
+    data['posts'] = post
+
     return render(request, 'post/list.html', data)
+
+def post_detalhes(request, pk):
+    data = {}
+    post = get_object_or_404(Post, pk = pk)
+    data['post'] = post
+
+    return render(request, 'post/detalhes.html', data)
