8935c913beed9774d54f3ad82e0cf0c554dbf694
Jhonathas César <jhonathascesar232@gmail.com>
2020-03-28 21:07:08 -0300

    Conclusão do tutorial djangoGirls

--- mySite/mySite/blog/urls.py
+++ mySite/mySite/blog/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.urls import path
-from .views import index, listar_post, post_detalhes
+from .views import index, listar_post, post_detalhes, novo_post, editar_post
 
 urlpatterns = [
     path('', index, name = 'index'),
     path('lista/', listar_post, name='post_list'),
+    path('post/novo/', novo_post, name='post_new'),
+    path('post/editar/<int:pk>/', editar_post, name='post_edit'),
     path('post-detalhes/<int:pk>/', post_detalhes, name='post_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mySite/mySite/blog/views.py
+++ mySite/mySite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
+from .forms import PostForm
+
 
 # Create your views here.
 def index(request):
@@ -20,3 +22,41 @@ def post_detalhes(request, pk):
     data['post'] = post
 
     return render(request, 'post/detalhes.html', data)
+
+def novo_post(request):
+    data = {}
+
+    if (request.method == 'POST'):
+        form = PostForm(request.POST)
+        if (form.is_valid()):
+            post = form.save(commit = False)
+            post.autor = request.user
+            post.data_pub = timezone.now()
+            post.save()
+            return post_detalhes(request, post.pk)
+    else:
+        form = PostForm()
+
+    data['form'] = form
+    return render(request, 'post/form.html', data)
+
+def editar_post(request, pk):
+    data = {}
+
+    post = get_object_or_404(Post, pk=pk)
+
+    if (request.method == 'POST'):
+        form = PostForm(request.POST, instance = post)
+
+        if (form.is_valid()):
+            post = form.save(commit = False)
+            post.autor = request.user
+            post.data_pub = timezone.now()
+            post.save()
+
+            return post_detalhes(request, post.pk)
+    else:
+        form = PostForm(instance = post)
+        data['form'] = form
+
+    return render(request, 'post/form.html', data)
